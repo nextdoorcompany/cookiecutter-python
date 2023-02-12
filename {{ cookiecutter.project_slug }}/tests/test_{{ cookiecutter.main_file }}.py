02d3804bb2253925df3f1b1fb99dca9827321f40
Justin Schechter <justin@nextdoorco.com>
2023-02-11 19:24:38 -0500

    Add major project files

---
+++ {{ cookiecutter.project_slug }}/tests/test_{{ cookiecutter.main_file }}.py
@@ -0,0 +1,3 @@
+import pytest
+
+import {{ cookiecutter.project_name}}.{{ cookiecutter.main_file}}
---
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_name }}/__main__.py
@@ -0,0 +1,10 @@
+from {{ cookiecutter.project_name}}.{{ cookiecutter.main_file}} import run
+
+__version__ = "0.0.1"
+
+
+def main():
+    run()
+
+if __name__ == "__main__":
+    main()
