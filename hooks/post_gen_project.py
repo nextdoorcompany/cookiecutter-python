ff2713d790f94955fbe4a4ecdfb62ad6c6c92bda
Justin Schechter <justin@nextdoorco.com>
2023-02-12 10:34:10 -0500

    Add hook to bootstrap project

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,46 @@
+import subprocess
+
+subprocess.run(["{{cookiecutter.python_exe}}", "-m", "venv", "env"])
+
+subprocess.run(
+    [
+        "env/{{cookiecutter.scripts_or_bin}}/python",
+        "-m",
+        "pip",
+        "install",
+        "-r",
+        "requirements-bootstrap.txt",
+        "-U",
+    ]
+)
+
+subprocess.run(
+    [
+        "env/{{cookiecutter.scripts_or_bin}}/pip-compile",
+        "--upgrade",
+        "--extra",
+        "dev",
+        "--output-file",
+        "requirements-dev.txt",
+        "--generate-hashes",
+        "--resolver",
+        "backtracking",
+        "pyproject.toml",
+    ]
+)
+
+subprocess.run(
+    [
+        "env/{{cookiecutter.scripts_or_bin}}/pip-sync",
+        "requirements-dev.txt",
+    ]
+)
+
+subprocess.run(
+    [
+        "git",
+        "init",
+        "-b",
+        "trunk",
+    ]
+)
